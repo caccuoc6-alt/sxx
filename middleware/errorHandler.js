cd29bdddd08e4c5cb61db2cb3290216044381c04
caccuoc6-alt <caccuoc6@gmail.com>
2026-04-03 00:02:55 +0700

    Create centralized error handling middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,8 @@
+// centralized error handling middleware
+
+function errorHandler(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!', error: err.message });
+}
+
+module.exports = errorHandler;
